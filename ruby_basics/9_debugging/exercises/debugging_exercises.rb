943a66be373d8d3bf6d6f47741cbca6afac48737
Virag Kormoczy <vkormoczy@gmail.com>
2022-12-28 18:29:02 +0200

    Fix methods after debugging

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -10,7 +12,7 @@ def decrement_smallest_value(nested_array)
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -21,10 +23,10 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
+  greatest_value = nested_array.flatten.max
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < 0
         greatest_value = current_value
       end
     end
@@ -37,7 +39,7 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split('')
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -51,6 +53,10 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+
+  name = name.upcase
+
   greeting = "WASSAP, #{name}!"
 end
+
+yell_greeting('maisy')
